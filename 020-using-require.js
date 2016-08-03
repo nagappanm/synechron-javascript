a2f7da24b736d277c1f616c90c83ab3eaa5452b8
vijay-tute <mail.technicaltraininer@gmail.com>
2016-08-03 14:14:25 +0530

    done

---
+++ 020-using-require.js
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>Modules Loading</title>
+	<script src="mods/hero.js"></script>
+	<script src="mods/walk.js"></script>
+	<script src="mods/run.js"></script>
+	<script src="mods/fly.js"></script>
+	<script src="mods/talk.js"></script>
+	<script>
+		console.log(hero.walk())
+		console.log(hero.run())
+	</script>
+</head>
+<body>
+
+</body>
+</html>
